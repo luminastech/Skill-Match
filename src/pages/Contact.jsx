fcb0a09237ee88d1116f71181ec4d9d5f4f62c4a
Manish Gupta <121728309+DevyManish@users.noreply.github.com>
2024-05-05 19:00:27 +0530

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -125,7 +125,7 @@ const Contact = () => {
                 Message
               </label>
               <textarea
-                className="flex w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 min-h-[120px]"
+                className="dark:text-black flex w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 min-h-[120px]"
                 id="message"
                 placeholder="Enter your message"
                 defaultValue={""}
